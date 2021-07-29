b2341a4feb3d96055d4ad00d8342ae0bc61142af
odwanodada <odwanodada22@gmail.com>
2021-07-29 16:52:31 +0200

    Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template> 
+    <header>
+        <h1>{{ title }}</h1>
+        
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default{
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components: {
+        Button,
+    },
+}
+</script>
+
+<style scoped>
+
+
+</style>
